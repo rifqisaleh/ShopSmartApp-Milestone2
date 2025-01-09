a7b8242569b53973978b7da483c65feaf5357a9c
rifqisaleh <mrifqisaleh@gmail.com>
2025-01-09 23:07:51 +0700

    move redirect to public

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set this to '/' for root-level deployment or '/subdirectory/' if deployed under a subdirectory
 })
